087b0befb8686368e92f9727d62212e12c62c01b
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-09-06 15:19:30 +0600

    add(utils): scroll to top when navigating a page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import "./App.css";
 import CommonLayout from "./components/layouts/CommonLayout";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <CommonLayout>
+      <ScrollToTop/>
       <Outlet />
     </CommonLayout>
   );
---
+++ src/utils/ScrollToTop.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    // window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
