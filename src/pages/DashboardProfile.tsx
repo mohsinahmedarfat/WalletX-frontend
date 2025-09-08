b0a82a0223fc4e8b89417c1c77858e76f1b5b4d5
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-09-08 16:15:33 +0600

    fix: dashboard profile dark issue

--- src/pages/DashboardProfile.tsx
+++ src/pages/DashboardProfile.tsx
@@ -13,8 +13,8 @@ const DashboardProfile = () => {
   const { data: user } = useUserInfoQuery(undefined);
 
   return (
-    <Card className="max-w-sm mx-auto border border-gray-200 shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <CardHeader className="flex flex-col items-center gap-2 bg-gradient-to-r from-blue-500 to-indigo-500 text-white rounded-t-lg p-4">
+    <Card className="max-w-sm mx-auto border border-gray-200 shadow-lg hover:shadow-xl transition-shadow duration-300 pt-0">
+      <CardHeader className="flex flex-col items-center gap-2 bg-gradient-to-r from-blue-500 to-indigo-500 text-white rounded-t-xl p-4">
         <Avatar className="w-20 h-20 border-2 border-white">
           <AvatarImage
             src={`https://ui-avatars.com/api/?name=${user?.data?.name}`}
@@ -27,15 +27,15 @@ const DashboardProfile = () => {
         <CardDescription className="text-white font-semibold">{user?.data?.email}</CardDescription>
       </CardHeader>
 
-      <CardContent className="flex flex-col gap-3 text-gray-700">
+      <CardContent className="flex flex-col gap-3 text-foreground">
         <div className="flex gap-2 items-center">
           <span className="font-medium">Role:</span>
           <Badge variant="secondary">{user?.data?.role}</Badge>
         </div>
 
-        <div className="flex justify-between items-center">
-          <span className="font-medium">Wallet ID:</span>
-          <span className="text-gray-900 font-medium">
+        <div className="flex justify-between items-center text-foreground font-medium">
+          <span className=" mr-2">Wallet ID:</span>
+          <span>
             {user?.data?.wallet || "N/A"}
           </span>
         </div>
