6cfc583d241c3df47401fa5ae33bdd51de44f8a0
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-09-08 16:51:41 +0600

    add: skeleton loader for profile in dashboard

--- src/pages/DashboardProfile.tsx
+++ src/pages/DashboardProfile.tsx
@@ -8,9 +8,33 @@ import {
 import { useUserInfoQuery } from "@/redux/feature/auth/auth.api";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const DashboardProfile = () => {
-  const { data: user } = useUserInfoQuery(undefined);
+  const { data: user, isLoading } = useUserInfoQuery(undefined);
+
+  if (isLoading) {
+    return (
+      <Card className="min-w-xs mx-auto border border-gray-200 shadow-lg pt-0">
+        <CardHeader className="flex flex-col items-center gap-2 bg-gradient-to-r from-blue-500 to-indigo-500 text-white rounded-t-xl p-4">
+          <Skeleton className="w-20 h-20 rounded-full border-2 border-white" />
+          <Skeleton className="h-6 w-32 rounded-md" />
+          <Skeleton className="h-4 w-48 rounded-md" />
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-3">
+          <div className="flex gap-2 items-center">
+            <Skeleton className="h-5 w-12 rounded-md" />
+            <Skeleton className="h-5 w-16 rounded-md" />
+          </div>
+          <div className="flex justify-between items-center">
+            <Skeleton className="h-5 w-20 rounded-md" />
+            <Skeleton className="h-5 w-28 rounded-md" />
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
 
   return (
     <Card className="max-w-sm mx-auto border border-gray-200 shadow-lg hover:shadow-xl transition-shadow duration-300 pt-0">
@@ -24,7 +48,9 @@ const DashboardProfile = () => {
         <CardTitle className="text-2xl font-semibold">
           {user?.data?.name}
         </CardTitle>
-        <CardDescription className="text-white font-semibold">{user?.data?.email}</CardDescription>
+        <CardDescription className="text-white font-semibold">
+          {user?.data?.email}
+        </CardDescription>
       </CardHeader>
 
       <CardContent className="flex flex-col gap-3 text-foreground">
@@ -35,9 +61,7 @@ const DashboardProfile = () => {
 
         <div className="flex justify-between items-center text-foreground font-medium">
           <span className=" mr-2">Wallet ID:</span>
-          <span>
-            {user?.data?.wallet || "N/A"}
-          </span>
+          <span>{user?.data?.wallet || "N/A"}</span>
         </div>
       </CardContent>
     </Card>
